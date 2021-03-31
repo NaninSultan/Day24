284452ead8d813fa01973e899311e56d89131b91
unknown <hamza.okanovic@hotmail.com>
2021-04-01 00:05:23 +0200

    Added new files

---
+++ ex03/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <button onClick={this.setLocalStorage}>Set Local Storage</button>
+    </div>
+    )
+  }
+
+  setLocalStorage = () => {
+    localStorage.setItem('Arena', 'Selection Month');
+  }
+}
+
+export default App;
