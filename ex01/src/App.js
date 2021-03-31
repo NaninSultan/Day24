ed2abd65123859531b1846340da68310668c56be
unknown <hamza.okanovic@hotmail.com>
2021-04-01 00:27:12 +0200

    Added new files

--- ex01/src/App.js
+++ ex01/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
-  setMyStorage = () => {
-    document.cookie = "Year=2021";
-    localStorage.setItem('Paragon', 'yes, but Arena first');
-    sessionStorage.setItem('frontend', 'React');
-  }
-  
   render() {
     return (
       <div className="App">
         <button onClick={this.setMyStorage}>Set my Storage</button>
       </div>
     )
+  }
+
+   setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem('Paragon', 'yes, but Arena first');
+    sessionStorage.setItem('frontend', 'React');
   }
 }
 
